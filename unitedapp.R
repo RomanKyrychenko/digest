7d58ac1cbbf4eeb0d4cb17c7278b0a01bb721317
RomanKyrychenko <kirichenko17roman@gmail.com>
2017-07-17 20:57:55 +0300

    upd

--- unitedapp.R
+++ unitedapp.R
@@ -1,3 +1,5 @@
+Sys.setlocale(,"UK_ua")
+
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
@@ -9,42 +11,11 @@ library(cowplot)
 library(png)
 library(extrafont)
 
-interp_points <- function (data) {
-  
-  df <- data.frame(line_id=c(),long=c(),lat=c())
-  
-  for (i in 1:nrow(data)) { 
-    
-    line <- data[i,]
-    
-    longseq <- seq(
-      as.numeric(line["tb2"]),
-      as.numeric(line["net2"]),
-      as.numeric((line["net2"] - line["tb2"])/10)
-    )
-    latseq <- seq(
-      as.numeric(line["tb"]),
-      as.numeric(line["net"]),
-      as.numeric(line["net"] - line["tb"])/10
-    )
-    
-    for (j in 1:10) {
-      df <- rbind(df,data.frame(line_id=i,long=longseq[j],lat=latseq[j],seg_num=j))
-    }
-  }
-  df$lat <- zoo::na.locf(df$lat)
-  for(i in unique(df$line_id)){
-    df <- rbind(df,c(i,1,data$net[i],11))
-  }
-  df
-}
-
 unzip(".fonts.zip",exdir = "~/",overwrite = T)
 system('fc-cache -f ~/.fonts')
-Sys.setlocale(,"UK_ua")
 
 ui <- dashboardPage(skin = "red",
-                    
+                    title="Corestone work tools",
                     dashboardHeader(
                       title="Corestone work tools",
                       tags$li(class = "dropdown",
@@ -111,7 +82,7 @@ ui <- dashboardPage(skin = "red",
                                 tags$hr(),
                                 downloadButton('down',"Завантажити в pdf!"),
                                 downloadButton('do',"Завантажити в png!"),
-                                plotOutput('plot', width = "1132px", height = "800px")
+                                plotOutput('plot', width = "3666px", height = "2358px")
                         )#,
                      #   tabItem(tabName = "sys2",
                      #           h2("System"),
@@ -122,26 +93,6 @@ ui <- dashboardPage(skin = "red",
 )
 
 server <- function(input,output,server,session){
-  #mem  <- reactive({system("% free | grep Mem | awk '{print $3/$2 * 100.0}'")})
-  #cpu <- reactive({system("top")})
-  #output$sys <- renderMenu({
-  #  dropdownMenu(type = "messages",
-  #               messageItem(from = "Memory",
-  #                           message = mem(),
-  #                           icon=icon("database"),
-  #                           time =Sys.time()),
-  #               messageItem(from = "CPU",
-  #                           message = cpu(),
-  #                           icon=icon("dashboard"),
-  #                           time =Sys.time())
-  #  )
-  #})
-  #ntext <- eventReactive(input$goButton, {
-  #  mem()
-  #})
-  #output$nText <- renderText({
-  #  ntext()
-  #})
   df <- reactive({
     inFile <- input$fl
     if(is.null(inFile))
@@ -174,20 +125,25 @@ server <- function(input,output,server,session){
     
     infoflow <- function(data){
       por <- c(9.9,8.85,7.8,6.75,5.7,4.65,3.6,2.55,1.5,.45)
-      lin <- inner_join(data_frame(name=data[[1]][14:23],tb=c(9.9,8.85,7.8,6.9,5.87,4.83,3.78,2.73,1.68,.63)),
-                        data_frame(name=data[[1]][2:11],net=c(9.9,8.85,7.8,6.9,5.87,4.83,3.78,2.73,1.68,.63)),by="name")
+      lin <- inner_join(data_frame(name=data[[1]][14:23],tb=#c(10,8.85,7.7,6.9,5.87,4.83,3.78,2.73,1.68,.63)),
+                                     seq(10,0.1,-1.1)),
+                        data_frame(name=data[[1]][2:11],net=#c(10,8.85,7.7,6.9,5.87,4.83,3.78,2.73,1.68,.63)),
+                                     seq(10,0.1,-1.1)),
+                        by="name")
       
       lin$tb2 <- 0
       lin$net2 <- 1
-      output <- interp_points(lin)
       
       p1 <- ggplot()+
         geom_bar(aes(reorder(data[[1]][2:11],as.numeric(data[[2]][2:11])),
-                     as.numeric(data[[3]][2:11])),stat = "identity",width = 0.6,fill="#bcbec0",color=NA)+
+                     as.numeric(data[[3]][2:11])),stat = "identity",width = 0.6,fill="#DFE0E1",color=NA)+
         geom_bar(aes(reorder(data[[1]][2:11],as.numeric(data[[2]][2:11])),
-                     as.numeric(data[[2]][2:11])),stat = "identity",width = 0.4,fill="#136278",color=NA)+
+                     as.numeric(data[[2]][2:11])),stat = "identity",width = 0.4,fill="#A2C1C1",color=NA)+
         geom_text(aes(reorder(data[[1]][2:11],as.numeric(data[[2]][2:11])),0,
-                      label=reorder(data[[1]][2:11],as.numeric(data[[2]][2:11]))),hjust=0,vjust=-2,size=6,family = "PT Sans")+
+                      label=reorder(data[[1]][2:11],as.numeric(data[[2]][2:11]))),hjust=0,vjust=-2,size=6*3,
+                  family = "PT Sans")+
+        annotate("text",0.3,max(as.numeric(data[[2]][2:11])),label="тис. контактів",
+                 hjust=1,vjust=-2,size=5*3,family = "PT Sans",color="#636566")+
         ylab("")+
         xlab("")+
         scale_x_discrete(labels=rev(data[[1]][2:11]),position = "top")+
@@ -195,41 +151,43 @@ server <- function(input,output,server,session){
         coord_flip() + theme(
           panel.grid = element_blank(), panel.border = element_blank(),
           axis.text.y = element_blank(),
-          axis.line.x = element_line(size=2,color="#136278"),
+          axis.line.x = element_line(size=1,color="#A2C1C1"),
           axis.line.y = element_blank(),
           axis.ticks = element_blank(),
           panel.background = element_blank(),
-          axis.text.x = element_text(hjust=0,vjust=-2,size=14,family = "PT Sans"),
-          axis.ticks.length = unit(0.2, "line"),
+          axis.text.x = element_text(hjust=0,vjust=-2,size=14*3,color="#636566",family = "PT Sans"),
+          axis.ticks.length = unit(0.1, "line"),
           #axis.ticks.length = unit(-1, "line"),
           plot.margin=unit(c(0,0,0,0), "cm")
         )
       
       p2 <- ggplot()+
         geom_bar(aes(reorder(data[[1]][14:23],as.numeric(data[[2]][14:23])),
-                     as.numeric(data[[3]][14:23])),stat = "identity",width = 0.6,fill="#bcbec0")+
+                     as.numeric(data[[3]][14:23])),stat = "identity",width = 0.6,fill="#DFE0E1")+
         geom_bar(aes(reorder(data[[1]][14:23],as.numeric(data[[2]][14:23])),
-                     as.numeric(data[[2]][14:23])),stat = "identity",width = 0.4,fill="#c41e58")+
+                     as.numeric(data[[2]][14:23])),stat = "identity",width = 0.4,fill="#E1B5AC")+
         geom_text(aes(reorder(data[[1]][14:23],as.numeric(data[[2]][14:23])),0,
-                      label=reorder(data[[1]][14:23],as.numeric(data[[2]][14:23]))),hjust=1,vjust=-2,size=6,family = "PT Sans")+
+                      label=reorder(data[[1]][14:23],as.numeric(data[[2]][14:23]))),hjust=1,vjust=-2,size=6*3,family = "PT Sans")+
+        annotate("text",0.3,max(as.numeric(data[[2]][14:23])),
+                 label="млн контактів",hjust=0,vjust=-2,size=5*3,family = "PT Sans",color="#636566")+
         ylab("")+
         xlab("")+
         scale_x_discrete(labels=rev(data[[1]][14:23]))+
         coord_flip() + theme(
           panel.grid = element_blank(), panel.border = element_blank(),
-          axis.line.x = element_line(color="#c41e58",size=2),
+          axis.line.x = element_line(color="#E1B5AC",size=1),
           axis.ticks = element_blank(),
           panel.background = element_blank(),
           axis.line.y = element_blank(),
           axis.text.y = element_blank(),
-          axis.text.x = element_text(hjust=1,vjust=-2,size=14,family = "PT Sans"),
-          axis.ticks.length = unit(0.2, "line"),
+          axis.text.x = element_text(hjust=1,vjust=-2,size=14*3,color="#636566",family = "PT Sans"),
+          axis.ticks.length = unit(0.1, "line"),
           plot.margin=unit(c(0,0,0,0), "cm")
         ) + scale_y_reverse(expand = c(0, 0))
       
-      p3 <- ggplot(output,aes(x=long,y=lat,group=line_id,color=seg_num)) + 
-        geom_path(alpha=1,size=2,lineend = "round") +
-        scale_color_gradient(high="#136278",low="#c41e58")+
+      p3 <- ggplot() + 
+        geom_segment(aes(x = lin$tb2, xend=lin$net2,y=lin$tb,yend=lin$net),color="#BFBFBF",alpha=1,size=0.5,lineend = "round") +
+        #scale_color_gradient(high="#136278",low="#c41e58")+
         ylab("")+
         xlab("")+
         scale_x_continuous(limits = c(0,1),expand = c(0, 0))+
@@ -243,18 +201,25 @@ server <- function(input,output,server,session){
           panel.background = element_blank(),
           axis.text = element_blank(),
           axis.ticks.length = unit(-1.4, "line"),
-          plot.margin=unit(c(0,0,0,0,0), "cm")
+          plot.margin=unit(c(0,-0.5,0,-0.5,0), "cm")
         ) 
       
       p4 <- ggplot()+
         geom_text(aes(1,por,label=ifelse(!is.na(as.numeric(data[[4]][14:23])),paste0(round(as.numeric(data[[4]][14:23])*100),"%"),"∞")),family = "PT Sans",
-                  hjust=1,color=ifelse(as.numeric(data[[4]][14:23])>0 | is.na(as.numeric(data[[4]][14:23])),"#136278","#c41e58"),size=6) +
-        geom_segment(aes(x=1.01,y=ifelse(is.na(as.numeric(data[[4]][14:23])) | as.numeric(data[[4]][14:23])>0,por-0.3,por+0.3),
-                         xend=1.01,yend=ifelse(is.na(as.numeric(data[[4]][14:23])) | as.numeric(data[[4]][14:23])>0,por+0.3,por-0.3)),
-                     arrow = arrow(length = unit(0.2, "cm")),size=1,color=ifelse(as.numeric(data[[4]][14:23])>0 | is.na(as.numeric(data[[4]][14:23])),"#136278","#c41e58"))+
+                  hjust=1,color="#636566",size=6*3) +
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][14]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[1]-0.1, ymax=por[1]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][15]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[2]-0.1, ymax=por[2]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][16]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[3]-0.1, ymax=por[3]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][17]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[4]-0.1, ymax=por[4]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][18]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[5]-0.1, ymax=por[5]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][19]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[6]-0.1, ymax=por[6]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][20]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[7]-0.1, ymax=por[7]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][21]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[8]-0.1, ymax=por[8]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][22]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[9]-0.1, ymax=por[9]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][23]>0,"green.png","red.png")), interpolate=TRUE), xmin=1, xmax=1.1, ymin=por[10]-0.1, ymax=por[10]+0.1)+
         ylab("")+
         xlab("")+
-        scale_x_continuous(limits = c(0.82,1.02))+
+        scale_x_continuous(limits = c(0.52,1.1))+
         scale_y_continuous(limits = c(0,10.5),expand = c(0, 0))+
         theme(
           legend.position = "none",
@@ -312,13 +277,20 @@ server <- function(input,output,server,session){
         geom_point(),ncol=1)
       p7 <- ggplot()+
         geom_text(aes(1,por,label=ifelse(!is.na(as.numeric(data[[4]][2:11])),paste0(round(as.numeric(data[[4]][2:11])*100),"%"),"∞")),family = "PT Sans",hjust=0,
-                  color=ifelse(as.numeric(data[[4]][2:11])>0,"#136278","#c41e58"),size=6) +
-        geom_segment(aes(x=0.99,y=ifelse(as.numeric(data[[4]][2:11])>0,por-0.3,por+0.3),
-                         xend=0.99,yend=ifelse(as.numeric(data[[4]][2:11])>0,por+0.3,por-0.3)),
-                     arrow = arrow(length = unit(0.2, "cm")),size=1,color=ifelse(as.numeric(data[[4]][2:11])>0,"#136278","#c41e58"))+
+                  color="#636566",size=6*3) +
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][2]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[1]-0.1, ymax=por[1]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][3]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[2]-0.1, ymax=por[2]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][4]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[3]-0.1, ymax=por[3]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][5]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[4]-0.1, ymax=por[4]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][6]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[5]-0.1, ymax=por[5]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][7]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[6]-0.1, ymax=por[6]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][8]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[7]-0.1, ymax=por[7]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][9]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[8]-0.1, ymax=por[8]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][10]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[9]-0.1, ymax=por[9]+0.1)+
+        annotation_custom(rasterGrob(readPNG(ifelse(data[[4]][11]>0,"green.png","red.png")), interpolate=TRUE), xmin=0.9, xmax=1, ymin=por[10]-0.1, ymax=por[10]+0.1)+
         ylab("")+
         xlab("")+
-        scale_x_continuous(limits = c(0.98,1.1))+
+        scale_x_continuous(limits = c(0.9,1.52))+
         scale_y_continuous(limits = c(0,10.5),expand = c(0, 0))+
         theme(
           legend.position = "none",
@@ -332,25 +304,15 @@ server <- function(input,output,server,session){
           plot.margin=unit(c(0,0,0,0,0), "cm")
         ) 
       
-      p8 <- ggplot() + 
-        annotation_custom(rasterGrob(readPNG("tv.png"), interpolate=TRUE), xmin=0, xmax=1, ymin=0.3, ymax=1) +
-        geom_text(aes(x=0.5,y=0.1,label=toupper("топ-персони доби на тб")),size=6) +
-        geom_point() + scale_y_continuous(limits = c(0,1)) + theme_void()
-      
-      p9 <- ggplot() + 
-        annotation_custom(rasterGrob(readPNG("net.png"), interpolate=TRUE), xmin=0, xmax=1, ymin=0.3, ymax=1) +
-        geom_text(aes(x=0.5,y=0.1,label=toupper("топ-персони доби в інтернеті")),size=6) +
-        geom_point() + scale_y_continuous(limits = c(0,1)) + theme_void()
-      
-      grid.arrange(p4,ggplot(),p6,ggplot(),p2,p3,p1,p5,ggplot(),p7,ggplot(),p8,p9,
-                   layout_matrix=cbind(c(11,11,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2), 
-                                       c(11,11,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4),
-                                       c(12,12,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5),
-                                       c(11,11,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6),
-                                       c(13,13,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7),
-                                       c(11,11,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,9),
-                                       c(11,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11)),
-                   widths=c(0.6,0.65,3,1,3,0.65,0.6))
+      grid.arrange(p2,p4,p6,p3,p5,p7,p1,ggplot(),
+                   layout_matrix=cbind(c(1,1,1,1,1,1,1,1,1,1,1,1,1), 
+                                       c(2,2,2,2,2,2,2,2,2,2,2,2,8),
+                                       c(3,3,3,3,3,3,3,3,3,3,3,3,8),
+                                       c(4,4,4,4,4,4,4,4,4,4,4,4,8),
+                                       c(5,5,5,5,5,5,5,5,5,5,5,5,8),
+                                       c(6,6,6,6,6,6,6,6,6,6,6,6,8),
+                                       c(7,7,7,7,7,7,7,7,7,7,7,7,7)),
+                   widths=c(3,0.67,0.65,1,0.65,0.67,3))
     }
     infoflow(data)
   })
@@ -375,7 +337,7 @@ server <- function(input,output,server,session){
       paste0("infoflow-",Sys.Date(),".png")
     },
     content = function(file) {
-      png(file, width=1240, height=750,bg = "white")
+      png(file, width=3666, height=2358,bg = "white")
       grid.draw(df2())
       dev.off()
     }
